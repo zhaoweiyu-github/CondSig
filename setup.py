106a7164a3f1dc28efc9fe7f63757002b96ec745
Zhaowei Yu <zhaoweiyu@Zhaoweis-MacBook-Pro.local>
2022-02-27 19:47:18 +0800

    First release

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def main():
 		package_dir = {"CondSig":"src"},
 		packages = ["CondSig"],
 		# packages = find_packages(where = "CondSig"),
-		scripts = ["bin/condsig", "bin/biterm_local.sh", "bin/btm"]
+		scripts = ["bin/condsig", "bin/biterm_local.sh"]
 		)
 
 if __name__ == "__main__":
